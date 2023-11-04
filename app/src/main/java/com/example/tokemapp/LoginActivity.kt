46c7b3e16748b5acef054be06b553470e70779a2
elsabhuwana <elsabhwnaura8@gmail.com>
2023-11-04 12:05:43 +0700

    Membuat Halaman Login Membuat Halaman Register

--- app/src/main/java/com/example/tokemapp/LoginActivity.kt
+++ app/src/main/java/com/example/tokemapp/LoginActivity.kt
@@ -1,43 +1,53 @@
-package com.example.tokemapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import com.example.tokemapp.MainActivity
+import com.example.tokemapp.R
 
-    class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
-        private lateinit var usernameEditText: EditText
-        private lateinit var passwordEditText: EditText
-        private lateinit var loginButton: Button
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var registerTextView: TextView
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_login)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
 
-            usernameEditText = findViewById(R.id.editTextUsername)
-            passwordEditText = findViewById(R.id.editTextPassword)
-            loginButton = findViewById(R.id.buttonLogin)
+        emailEditText = findViewById(R.id.editTextEmail)
+        passwordEditText = findViewById(R.id.editTextPassword)
+        loginButton = findViewById(R.id.buttonLogin)
+        registerTextView = findViewById(R.id.textViewTitle4)
 
-            loginButton.setOnClickListener {
-                val username = usernameEditText.text.toString()
-                val password = passwordEditText.text.toString()
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
 
-                if (isValidCredentials(username, password)) {
-                    // Jika login berhasil, pindah ke aktivitas berikutnya
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                } else {
-                    // Jika login gagal, tampilkan pesan kesalahan
-                    Toast.makeText(this, "Login gagal. Coba lagi.", Toast.LENGTH_SHORT).show()
-                }
+            if (isValidCredentials(email, password)) {
+                // Jika login berhasil, pindah ke aktivitas berikutnya
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                // Jika login gagal, tampilkan pesan kesalahan
+                Toast.makeText(this, "Login gagal. Coba lagi.", Toast.LENGTH_SHORT).show()
             }
         }
 
-        // Fungsi untuk melakukan validasi username dan password (contoh sederhana)
-        private fun isValidCredentials(username: String, password: String): Boolean {
-            return username == "user" && password == "password"
+        registerTextView.setOnClickListener {
+            // Pindah ke halaman registrasi jika ditekan
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
+
+    private fun isValidCredentials(email: String, password: String): Boolean {
+        // Implementasikan validasi email dan password sesuai kebutuhan Anda
+        // Anda bisa melakukan validasi dengan membandingkan email dan password dengan data yang benar
+        return email == "contoh@email.com" && password == "password123"
+    }
+}
---
+++ app/src/main/java/com/example/tokemapp/RegisterActivity.kt
@@ -0,0 +1,46 @@
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.tokemapp.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var confirmPasswordEditText: EditText
+    private lateinit var registerButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        emailEditText = findViewById(R.id.editTextEmail)
+        passwordEditText = findViewById(R.id.editTextPassword)
+        confirmPasswordEditText = findViewById(R.id.editTextConfirmPassword)
+        registerButton = findViewById(R.id.buttonRegister)
+
+        registerButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+            val confirmPassword = confirmPasswordEditText.text.toString()
+
+            if (isValidRegistration(email, password, confirmPassword)) {
+                // Simpan data pendaftaran atau pindah ke aktivitas lain
+                Toast.makeText(this, "Pendaftaran berhasil", Toast.LENGTH_SHORT).show()
+                // Tambahkan logika untuk menyimpan data pengguna ke database atau backend di sini
+            } else {
+                // Jika pendaftaran gagal, tampilkan pesan kesalahan
+                Toast.makeText(this, "Pendaftaran gagal. Pastikan semua isian benar.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isValidRegistration(email: String, password: String, confirmPassword: String): Boolean {
+        // Implementasikan validasi email, password, dan konfirmasi password sesuai kebutuhan Anda
+        // Anda bisa melakukan validasi seperti memeriksa apakah email valid dan apakah password dan konfirmasi password sesuai
+        return email.isNotEmpty() && password.isNotEmpty() && password == confirmPassword
+    }
+}
