c3943e652bd175ddf83800bae553b906533d6de7
elsabhuwana <elsabhwnaura8@gmail.com>
2023-11-04 12:09:38 +0700

    Membuat Halaman Login Membuat Halaman Register Membuat class login Membuat class regis

--- app/src/main/java/com/example/tokemapp/RegisterActivity.kt
+++ app/src/main/java/com/example/tokemapp/RegisterActivity.kt
@@ -13,6 +13,7 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var confirmPasswordEditText: EditText
     private lateinit var registerButton: Button
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
