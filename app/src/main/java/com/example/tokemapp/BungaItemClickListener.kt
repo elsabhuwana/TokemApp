3cc8b0069ffc3e11765f63dfb56cb938d877b37b
elsabhuwana <elsabhwnaura8@gmail.com>
2023-12-04 15:53:13 +0700

    activity, gradle

--- app/src/main/java/com/example/tokemapp/BungaItemClickListener.kt
+++ app/src/main/java/com/example/tokemapp/BungaItemClickListener.kt
@@ -4,6 +4,7 @@ interface BungaItemClickListener {
     fun onItemClick(flower: ListBunga, quantity: Int)
 }
 
+
 open class BungaItemClickListenerImpl : BungaItemClickListener {
     override fun onItemClick(flower: ListBunga, quantity: Int) {
         // Implementasi logika ketika item diklik
